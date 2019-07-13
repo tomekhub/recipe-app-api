6058ba86d3bf3c2658ec87d796c7af270d55e2a2
thomasschopinski <thomasschopinski@gmail.com>
2019-07-13 14:24:38 +0200

    add manage user endpoints

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Recipes API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
+    path('docs/', schema_view)
 ]
